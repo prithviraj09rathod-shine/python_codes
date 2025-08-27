97ca0d62b3498b5db29126d7cc98d87b335ad229
Reshma A Pawar <prithviraj09rathod@gmail.com>
2025-08-27 17:43:14 +0530

    update basic concepts

--- oops_concpets.py
+++ oops_concpets.py
@@ -18,14 +18,16 @@ class truck(vehicle):
 class bus(vehicle):
     def __init__(self, brand, model):
         super().__init__(brand, model)
-        self.level = "4 wheeler"
+        self.level = "8 wheeler"
 
 '''polymorphism example when different classes have same method name move()
 and executed particular move() based on, which object is calling it'''
 class car:
-    def __init__(self, brand, model):
-        super().__init__(brand, model)
-        self.level = "4 wheeler"
+    def __init__(self, brand, model) :
+        self.brand = brand
+        self.model = model  
+    def move(self):
+        print("Move!")
 
 class Boat:
     def __init__(self, brand, model) :
@@ -40,6 +42,11 @@ class Plane:
         self.model = model  
     def move(self):
         print("Fly!")
+
+#inherited class object creation
+moto1 = motorcycle("Harley-Davidson", "Street 750") #Create a
+truck1 = truck("Ford", "F-150")   #Create a Truck object
+bus1 = bus("Volvo", "7700")        #Create a Bus object
 
 car1 = car("Ford", "Mustang")       #Create a Car object
 boat1 = Boat("Ibiza", "Touring 20") #Create a Boat object   
